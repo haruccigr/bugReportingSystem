7a8639432b445c718a1f720b71d5805c04b42c34
Harry <harry.siarras@gmail.com>
2019-05-31 15:36:19 +0300

    fixed bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BugsListModule } from './modules/bugs-list/bugs-list.module';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
