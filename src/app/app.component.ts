3a3fac108ed62bc42831ec79dd57b31ffe67f2b1
Harry <harry.siarras@gmail.com>
2019-06-15 22:50:46 +0300

    colours changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,31 +8,37 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'bugReportingSystem';
   bgMode: string;
-
+  private static white = 'linear-gradient(white, gray)';
+  private static dark = 'linear-gradient(rgb(82, 82, 82), rgb(32, 32, 32))';
+ 
   constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
     let mode = localStorage.getItem('bgMode');
 
     if (mode === null || mode === undefined) {
-      this.bgMode = 'white';
+      this.bgMode = AppComponent.white;       // vibrant color
     } else {
       this.bgMode = mode;
-      this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = this.bgMode;
+      this.elementRef.nativeElement.ownerDocument.body.style.backgroundImage = this.bgMode;
     }
   }
 
   toggleDarkMode(): void {
-    let darkColor = '#303030';
+   // let darkColor = 'linear-gradient(gray, black)';
+    // '#303030';
+    
 
-    if (this.bgMode === darkColor) {
-      this.bgMode = 'white';
+    if (this.bgMode === AppComponent.dark) {
+      this.bgMode = AppComponent.white;
     }
     else {
-      this.bgMode = darkColor;
+      this.bgMode = AppComponent.dark;
     }
     localStorage.setItem('bgMode', this.bgMode);
-    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = this.bgMode;
+    //this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = this.bgMode;
+    //background-image: linear-gradient(red, yellow);
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundImage = this.bgMode;
   }
 
 
